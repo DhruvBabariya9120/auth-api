93df82a9a72ba24c9c6826864553c4121816a852
dhruv <dhruvbabariya912001@gmail.com>
2025-03-24 22:37:01 +0530

    resolved issue

--- index.js
+++ index.js
@@ -141,7 +141,7 @@ app.post('/login', async (req, res) => {
     }
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
     console.log(`Swagger documentation available at http://localhost:${PORT}/api-docs`);
